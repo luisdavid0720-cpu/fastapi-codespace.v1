80b66767908d285763a24ca77fe3b2a102e94381
samuel5005 <samuelquesadacp@gmail.com>
2026-02-26 02:51:58 +0000

    Lo que hice

--- FAST-API/app/config/db_config.py
+++ FAST-API/app/config/db_config.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_db_connection():
     return psycopg2.connect(
-        host="ep-billowing-smoke-ai5rf94d-pooler.c-4.us-east-1.aws.neon.tech",
+        host="ep-young-sunset-aid708e7-pooler.c-4.us-east-1.aws.neon.tech",
         port="5432",
         user="neondb_owner",
-        password="npg_IBaq6Q3cgJvl",
+        password="npg_M8kwjLFHVdN7",
         dbname="neondb"
     )
