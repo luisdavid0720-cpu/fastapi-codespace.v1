cda4dc2e3132b5a40ae167588bcec3dcecea76ac
jurgen2931 <jurgenklisman2002@gmail.com>
2026-03-01 06:31:33 +0000

    Lo que hice

--- FAST-API/app/models/historial_estado_model.py
+++ FAST-API/app/models/historial_estado_model.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-from datetime import date
+from datetime import datetime
+from typing import Optional
 
 class Historial_estado(BaseModel):
-    id_historial: int=None
-    fecha: date
-    id_pqrs: int
+    id_historial: Optional[int] = None
+    fecha: Optional[datetime] = None
+    id_pqr: int
     id_estado: int
-   
