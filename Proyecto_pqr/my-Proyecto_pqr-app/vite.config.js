96fcaa8d15697e2065590ee689c68e44e6aea04e
Jurgen-cantillo <87295518+Jurgen-cantillo@users.noreply.github.com>
2026-03-20 19:06:52 +0000

    Lo que hice

--- Proyecto_pqr/my-Proyecto_pqr-app/vite.config.js
+++ Proyecto_pqr/my-Proyecto_pqr-app/vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/get_': { target: 'http://localhost:8000', changeOrigin: true },
+      '/create_': { target: 'http://localhost:8000', changeOrigin: true },
+      '/update_': { target: 'http://localhost:8000', changeOrigin: true },
+      '/delete_': { target: 'http://localhost:8000', changeOrigin: true },
+    }
+  }
 });
