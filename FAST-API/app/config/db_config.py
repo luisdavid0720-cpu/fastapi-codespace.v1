3fa94508eadec223097323ee539a3903237c849f
luisdavidangarita20-source <luisdavidangarita20@gmail.com>
2026-04-23 05:43:48 +0000

    dbconfig error

--- FAST-API/app/config/db_config.py
+++ FAST-API/app/config/db_config.py
@@ -4,7 +4,7 @@ import os
 def get_db_connection():
     return psycopg2.connect(
         host=os.getenv("NEON_HOST"),
-        port="5432",
+        port=os.getenv("NEON_PORT", "5432"),
         user=os.getenv("NEON_USER"),
         password=os.getenv("NEON_PASSWORD"),
         dbname=os.getenv("NEON_DBNAME"),
