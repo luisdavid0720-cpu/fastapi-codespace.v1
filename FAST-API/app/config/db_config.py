be23491ccb0e31f3c59e573974b8272d0619b26b
Jurgen-cantillo <87295518+Jurgen-cantillo@users.noreply.github.com>
2026-04-23 05:56:40 +0000

    Lo que hice

--- FAST-API/app/config/db_config.py
+++ FAST-API/app/config/db_config.py
@@ -3,10 +3,11 @@ import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("ep-young-sunset-aid708e7-pooler.c-4.us-east-1.aws.neon.tech"),
+        host=os.getenv("NEON_HOST"),
         port="5432",
-        user=os.getenv("neondb_owner"),
-        password=os.getenv("npg_M8kwjLFHVdN7"),
-        dbname=os.getenv("neondb"),
-        sslmode="require"
+        user=os.getenv("NEON_USER"),
+        password=os.getenv("NEON_PASSWORD"),
+        dbname=os.getenv("NEON_DBNAME"),
+        sslmode="require",
+        options="-c channel_binding=require"
     )
