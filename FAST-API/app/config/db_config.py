72a15c1443d256c9360cb9141b0af5a2cbe4ea82
Jurgen-cantillo <87295518+Jurgen-cantillo@users.noreply.github.com>
2026-04-23 05:07:50 +0000

    fix: corregir pqr_controller

--- FAST-API/app/config/db_config.py
+++ FAST-API/app/config/db_config.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return psycopg2.connect(
         host=os.getenv("ep-young-sunset-aid708e7-pooler.c-4.us-east-1.aws.neon.tech"),
         port="5432",
-        user=os.getenv(" neondb_owner"),
+        user=os.getenv("neondb_owner"),
         password=os.getenv("npg_M8kwjLFHVdN7"),
         dbname=os.getenv("neondb"),
         sslmode="require"
